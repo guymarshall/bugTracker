abe1f4652c80138c25164e904bd150ef8bf4effb
Guy Marshall <guymarshall.uk@gmail.com>
2022-05-21 23:02:14 +0100

    Add comment for code to add

---
+++ src/Bug.java
@@ -0,0 +1,16 @@
+public class Bug {
+}
+
+/*internal class Bug
+{
+    public int BugID { get; set; }
+    public string Status { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public DateTime DateCreated { get; set; }
+    public DateTime DateUpdated { get; set; }
+    public string CreatedBy { get; set; }
+    public string AssignedTo { get; set; }
+    public string Priority { get; set; }
+    public string Category { get; set; }
+}*/
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+
+        Bug bug = new Bug();
+
+        System.out.println(bug);
     }
 }
