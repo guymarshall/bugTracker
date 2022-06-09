7a111cb25fa1bd91c6e6daeb4fd2785aecd05367
Guy Marshall <guy@cslgroupservices.co.uk>
2022-06-09 12:08:59 +0100

    Move over files from IntelliJ project

---
+++ src/main/java/com/marshall/guy/BugFileHandler.java
@@ -0,0 +1,30 @@
+package com.marshall.guy;
+
+import java.io.FileWriter;
+import java.io.IOException;
+
+//serialise object
+
+public class BugFileHandler {
+    private static final String SEPARATOR = "|%$&£**&!*";
+    public String generateFileContents(String[] bugDetails) {
+        StringBuilder fileContents = new StringBuilder();
+
+        for (String bugDetail: bugDetails) {
+            fileContents.append(bugDetail).append(SEPARATOR);
+        }
+        return fileContents.toString();
+    }
+    public void saveFile(String fileName, String fileContents) {
+        String extension = ".txt";
+        try (FileWriter myWriter = new FileWriter(fileName + extension)) {
+            myWriter.write(fileContents);
+            System.out.println("Contents have been saved to file.");
+        } catch (IOException e) {
+            System.out.println("An error occurred.");
+            e.printStackTrace();
+        }
+    }
+
+    /*readFile()*/
+}
